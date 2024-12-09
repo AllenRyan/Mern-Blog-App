2b8dd641cc4119497e9e386d3b8ab2891b0d45ba
AllenRyan <allenisop786@gmail.com>
2024-12-09 12:21:29 +0500

    improved routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,46 +1,31 @@
-import {createBrowserRouter, RouterProvider} from "react-router-dom"
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import About from "./pages/About.jsx";
 import Signin from "./pages/Signin.jsx";
 import Signup from "./pages/Signup.jsx";
 import Dashboard from "./pages/Dashboard.jsx";
 import Projects from "./pages/Projects.jsx";
+import Layout from "./pages/Layout.jsx";
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <Home />
-  },
-  {
-    path: '/about',
-    element: <About />
-  },
-  {
-    path: '/sign-in',
-    element: <Signin />
-  },
-  {
-    path: '/sign-up',
-    element: <Signup />
-  },
-  {
-    path: '/dashboard',
-    element: <Dashboard />
-  },
-  {
-    path: '/projects',
-    element: <Projects />
+    element: <Layout />, // Use Layout as the root element
+    children: [
+      { path: '', element: <Home /> },
+      { path: 'about', element: <About /> },
+      { path: 'sign-in', element: <Signin /> },
+      { path: 'sign-up', element: <Signup /> },
+      { path: 'dashboard', element: <Dashboard /> },
+      { path: 'projects', element: <Projects /> }
+    ]
   }
+]);
 
-])
 function App() {
-  return(
-    <>  
-    <RouterProvider router={router}>
-
-    </RouterProvider>
-    </>
+  return (
+    <RouterProvider router={router} />
   );
 }
 
-export default App
+export default App;
---
+++ client/src/pages/Layout.jsx
@@ -0,0 +1,14 @@
+// Layout.jsx
+import { Outlet } from "react-router-dom";
+import Header from "../components/Header.jsx";
+
+function Layout() {
+  return (
+    <>
+      <Header />
+      <Outlet />
+    </>
+  );
+}
+
+export default Layout;
