bac16cffde46a34cd4644999dd2db148a1a04ea5
AllenRyan <allenisop786@gmail.com>
2024-12-13 11:23:32 +0500

    complete update user functionality

--- Api/controllers/user.controller.js
+++ Api/controllers/user.controller.js
@@ -3,9 +3,11 @@ import { errorHandler } from "../utils/error.js";
 import bycriptjs from "bcryptjs";
 
 export const updateUser = async (req, res, next) => {
+   
     if(req.user.id != req.params.userId){
         return next(errorHandler(403, 'You are not allowed to update this user'))
     }
+    
     
     if(req.body.password){
         if(req.body.password.length < 6){
@@ -13,6 +15,7 @@ export const updateUser = async (req, res, next) => {
         }
     }
     req.body.password = bycriptjs.hashSync(req.body.password, 10);
+
     if(req.body.username){
         if(req.body.username.length < 7 || req.body.username.length > 20 ){
             return next(errorHandler(400, 'username must be between 7 and 20 characters'))
@@ -27,6 +30,7 @@ export const updateUser = async (req, res, next) => {
     if (!req.body.username.match(/^[a-zA-Z0-9]+$/)){
          return next(errorHandler(400, 'username can only contain numbers and letters'))
     }
+    }
     try {
         let updatedUser = await User.findByIdAndUpdate(req.params.userId, {
             $set: {
@@ -40,5 +44,5 @@ export const updateUser = async (req, res, next) => {
     } catch (error) {
         next(error)
     }
-}
+   
 }
--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -1,12 +1,45 @@
 import { Button, TextInput } from 'flowbite-react'
 import { useEffect, useRef, useState } from 'react'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux';
+import { updateStart, updateSuccess, updateFailure, signInSuccess, signInFailure} from '../Redux/user/userSlice';
 
 function DashProfile() {
     const {currentUser} = useSelector(state => state.user);
     const [imageFile, setImageFile] = useState(null);
     const [imageFileUrl, setImageFileUrl] = useState(null);
-    const filePickerRef = useRef()
+    const filePickerRef = useRef();
+    const [formData, setFormData] = useState({});
+    const dispatch = useDispatch();
+    const handleChange =  (e) => {
+      setFormData({...formData, [e.target.id]: e.target.value})
+    }
+
+    const handleSubmit = async (e) => {
+      e.preventDefault();
+      if(Object.keys(formData).length === 0 ){
+          return;
+      }
+      try {
+        dispatch(updateStart());
+        const res = await fetch(`/api/user/update/${currentUser._id}`,{
+          method: "PUT",
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(formData)
+        });
+        console.log(formData)
+        const data = await res.json();
+        if(!res.ok){
+          dispatch(updateFailure(data.message))
+        }else{
+          dispatch(updateSuccess(data))
+        }
+      } catch (error) {
+        dispatch(updateFailure())
+      }
+    }
+
    const handleImageChange = (e) => {
     const file = e.target.files[0];
     if(file){
@@ -18,19 +51,19 @@ function DashProfile() {
       uploadImage()
     },[imageFile]);
     const uploadImage = async () => {
-      console.log('uploading image');
+      console.log('uploading image')
     };
   return (
     <div className='max-w-lg mx-auto p-3 w-full'>
         <h1 className='my-7 text-center font-semibold text-3xl'>Profile</h1>
-        <form className='flex flex-col gap-6 px-6'>
+        <form onSubmit={handleSubmit} className='flex flex-col gap-6 px-6'>
           <input hidden ref={filePickerRef} onChange={handleImageChange} type="file"  accept='image/*'/>
             <div className='w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full'>
             <img onClick={() => filePickerRef.current.click()} className='rounded-full w-full h-full border-8 border-[lightgray] object-cover' src={imageFileUrl || currentUser.profilePic} alt="profilePic" />
             </div>
-            <TextInput type='text' id='username' placeholder='username' defaultValue={currentUser.username}></TextInput>
-            <TextInput type='email' id='email' placeholder='email' defaultValue={currentUser.email}></TextInput>
-            <TextInput type='password' id='password' placeholder='password'></TextInput>
+            <TextInput type='text' id='username' placeholder='username' defaultValue={currentUser.username} onChange={handleChange}></TextInput>
+            <TextInput type='email' id='email' placeholder='email' defaultValue={currentUser.email} onChange={handleChange}></TextInput>
+            <TextInput type='password' id='password' placeholder='password' onChange={handleChange}></TextInput>
             <Button gradientDuoTone='purpleToBlue' type='submit' outline>Update</Button>
         </form>
         <div className='text-red-500 cursor-pointer flex justify-between mt-5 px-6 '>
