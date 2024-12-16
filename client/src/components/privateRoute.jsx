cd71f0ebbec39155f3e4c0783424a0689ba353a2
AllenRyan <allenisop786@gmail.com>
2024-12-16 10:25:31 +0500

    add a create post ui

--- client/src/components/privateRoute.jsx
+++ client/src/components/privateRoute.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { Outlet, Navigate } from 'react-router-dom';
+import {  Navigate } from 'react-router-dom';
 
 function PrivateRouter({children}) {
     const {currentUser} = useSelector(state => state.user);
