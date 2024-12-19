548c771952c3526d1105d89d05af038791d7668c
AllenRyan <allenisop786@gmail.com>
2024-12-19 11:07:18 +0500

    add like functionality to the comment component

--- client/src/components/CommentSection.jsx
+++ client/src/components/CommentSection.jsx
@@ -1,7 +1,7 @@
 import { Alert, Button, Textarea} from 'flowbite-react'
 import React, { useEffect, useState } from 'react'
 import {useSelector} from 'react-redux'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import Comments from './Comments'
 
 function CommentSection({postId}) {
@@ -9,6 +9,8 @@ function CommentSection({postId}) {
     const [comment, setComment] = useState('')
     const [commentError, setCommentError] = useState(null)
     const [comments, setComments] = useState([])
+    console.log(comments)
+    const navigate = useNavigate();
     const handleSubmit = async (e) => {
         e.preventDefault();
         if(comment.length > 200){
@@ -22,7 +24,7 @@ function CommentSection({postId}) {
                 },
                 body: JSON.stringify({content: comment, postId, userId: currentUser._id})
             });
-            const data = await res.json();
+            const data = await res.json()
             if(res.ok){
                 setComment('')
                 setCommentError(null)
@@ -49,6 +51,30 @@ function CommentSection({postId}) {
       }
       getComments()
     },[postId])
+
+    const handleLikes = async (commentId) => {
+      try {
+        if(!currentUser){
+         navigate('/sign-in');
+         return;
+        }
+        const res = await fetch(`/api/comments/likeComment/${commentId}`, {
+          method: "PUT",
+        });
+        if(res.ok){
+          const data = await res.json();
+          setComments(comments.map((comment) => (
+            comment._id === commentId ? {
+              ...comment,
+              likes: data.likes,
+              numberOfLikes: data.likes.length,
+            } : comment
+          )))
+        }
+      } catch (error) {
+        console.log(error.message)
+      }
+    }
   return (
     <div className='max-w-2xl mx-auto w-full p-3'>
       {currentUser ? (
@@ -98,7 +124,7 @@ function CommentSection({postId}) {
           </div>
           {
             comments.map(comment => (
-              <Comments key={comment._id} comment={comment} />
+              <Comments key={comment._id} comment={comment} onLike={handleLikes}/>
             ))
           }
           </>
